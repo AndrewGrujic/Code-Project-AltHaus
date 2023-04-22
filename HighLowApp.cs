ebf5bbee9b74e2ea2ce40015ca79a422e8b18298
OneUpAndy <78950797+OneUpAndy@users.noreply.github.com>
2023-04-22 13:49:13 +0100

    Fixed some some bugs & added an app exit.

--- HighLowApp.cs
+++ HighLowApp.cs
@@ -20,7 +20,7 @@ namespace Code_Project_AltHaus
 
             Console.WriteLine("Welcome to Higher or Lower");
             Console.WriteLine();
-
+        HighLowStart:
             int userHints = SetHints();
             int number = NumberSet();
 
@@ -28,24 +28,28 @@ namespace Code_Project_AltHaus
             Console.WriteLine("I am thinking of a number between 1 and 20. I will tell you if it is higher or lower.");
             Console.WriteLine($"You have {userHints} hints and then you must guess the number.");
 
-            int guess = SetGuess();
+            int guess = 0;
 
-            while (userHints > 0 || guess == number) {
+            while (userHints != 0 && guess != number)
+            {
+                guess = SetGuess();
+
                 switch (NumberGuess(guess,number))
                 {
                     case GuessState.Higher:
+                        Console.Clear();
                         Console.WriteLine("The number is higher than your guess.");
                         userHints--;
-                        SetGuess();
                         break;
 
                     case GuessState.Lower:
+                        Console.Clear();
                         Console.WriteLine("The number is lower than your guess.");
                         userHints--;
-                        SetGuess();
                         break;
 
                     case GuessState.Correct:
+                        Console.Clear();
                         Console.WriteLine($"Correct! The number is {number}!");
                         break;
                 }
@@ -58,12 +62,41 @@ namespace Code_Project_AltHaus
 
                 if (guess == number)
                 {
+                    Console.Clear();
                     Console.WriteLine($"Correct! The number is {number}!");
                 }
                 else
                 {
+                    Console.Clear();
                     Console.WriteLine($"Sorry, the correct number was {number}");
                 }
+            }
+
+        SelectionRetry:
+            Console.WriteLine("Would you like to play again?");
+            Console.WriteLine("0: Quit Higher or Lower");
+            Console.WriteLine("1: Play another game of Higher or Lower");
+
+            //Parses and validates the selection on if the user wants to quit out or retry.
+            bool isValid = Int32.TryParse(Console.ReadLine(), out int selection);
+            if (selection < 0 || !isValid)
+            {
+                Console.WriteLine("This is not a valid selection, please try again.");
+                goto SelectionRetry;
+            }
+
+            switch (selection)
+            {
+                case 0:
+                    Console.Clear();
+                    return;
+                case 1:
+                    Console.Clear();
+                    goto HighLowStart;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("This is not a valid selection, please try again.");
+                    goto SelectionRetry;
             }
 
         }
@@ -76,8 +109,8 @@ namespace Code_Project_AltHaus
 
         private static GuessState NumberGuess(int guess, int number)
         {
-            if (guess > number) { return GuessState.Higher; }
-            else if (guess < number) { return GuessState.Lower; }
+            if (guess < number) { return GuessState.Higher; }
+            else if (guess > number) { return GuessState.Lower; }
             else{return GuessState.Correct;}
         }
 
@@ -86,7 +119,7 @@ namespace Code_Project_AltHaus
             Console.WriteLine("Please make a guess:");
 
             bool guessCheck = Int32.TryParse(Console.ReadLine(), out int userGuess);
-            while (!guessCheck && userGuess > 0)
+            while (!guessCheck || userGuess < 1)
             {
                 Console.WriteLine("This is not a valid guess to enter, please try again.");
                 guessCheck = Int32.TryParse(Console.ReadLine(), out userGuess);
@@ -96,11 +129,11 @@ namespace Code_Project_AltHaus
 
         private static int SetHints()
         {
-            Console.WriteLine("Please put how many hint you want before you guess:");
+            Console.WriteLine("Please put how many hints you want before you guess:");
 
 
             bool hintCheck = Int32.TryParse(Console.ReadLine(), out int userHints);
-            while (!hintCheck && userHints < 0)
+            while (!hintCheck || userHints < 0)
             {
                 Console.WriteLine("This is not a valid number of hints to set. Please try again.");
                 hintCheck = int.TryParse(Console.ReadLine(), out userHints);
